70e560dfa3ab7f428764a22b4cbc423777aaa166
Abdqadir <masubaabdq@gmail.com>
2025-07-18 06:13:02 +0300

    modfications

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/headlight-infant/", // 👈 required for GitHub Pages
+  base: "/headlight-infant", // 👈 required for GitHub Pages
   server: {
     host: "::",
     port: 8080,
