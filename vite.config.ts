40307b37775a95e26939a798f93707539314b50b
abdqadir <masubaabdq@gmail.com>
2025-07-28 08:31:58 +0300

    git test 3

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "./headlight-infant",
+  base: "/headlight-infant",
   plugins: [
     react()
   ],
