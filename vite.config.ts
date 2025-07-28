97ddef739b051fd2608f509cfdf070955b8ea7b5
abdqadir <masubaabdq@gmail.com>
2025-07-28 08:01:43 +0300

    deployment base path?

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // base: "/headlight-infant/",
+  base: "/headlight-infant/",
   server: {
     host: "::",
     port: 3001,
