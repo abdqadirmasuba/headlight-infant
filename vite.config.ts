ec64e9cbf66bb08712ebeae5c29bfe4e5c52a08a
abdqadir <masubaabdq@gmail.com>
2025-07-28 08:23:17 +0300

    test with ./

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/headlight-infant/",
-  server: {
-    host: "::",
-    port: 3001,
-  },
+  base: "./headlight-infant",
   plugins: [
     react()
   ],
